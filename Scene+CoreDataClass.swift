44a83ccb20f2cf4345e2029b3dfe3e6fe8c20a37
evyhsiao <104025306+evyhsiao@users.noreply.github.com>
2022-04-22 10:17:56 +0800

    Add files via upload

---
+++ Scene+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Scene+CoreDataClass.swift
+//  Final
+//
+//  Created by evyhsiao on 2021/12/29.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Scene: NSManagedObject {
+
+}
---
+++ Scene+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+////
+////  Scene+CoreDataProperties.swift
+////  Final
+////
+////  Created by evyhsiao on 2021/12/29.
+////
+////
+//
+//import Foundation
+//import CoreData
+//
+//
+//extension Scene {
+//
+//    @nonobjc public class func fetchRequest() -> NSFetchRequest<Scene> {
+//        return NSFetchRequest<Scene>(entityName: "Scene")
+//    }
+//
+//    @NSManaged public var name: String?
+//    @NSManaged public var city: String?
+//    @NSManaged public var summary: String?
+//
+//}
+//
+//extension Scene : Identifiable {
+//
+//}
